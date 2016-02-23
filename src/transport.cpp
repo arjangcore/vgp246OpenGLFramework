4a2e06eb439922bf7e129914f962ad8b2c167002
Reza Nezami <arjangcore@gmail.com>
2016-02-23 10:23:06 -0800

    reapply previous change

--- src/transport.cpp
+++ src/transport.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <vector>
 
 #ifdef WIN32
 #include <windows.h>
@@ -22,9 +23,8 @@
 //#include "vector2d.h"
 #include "vector3d.h"
 #include  "matrices.h"
-using namespace std;
 
-typedef enum 
+typedef enum
 {
 	eStop = -1,
 	eIdle = 0,
@@ -35,40 +35,49 @@ typedef enum
 	eAngleDown,
 } changeType;
 
-double friction = 0.1; // this is friction coefficient between the sphere and the slope.
-double airResistance = 0.1; //
-double gravity = 9.81; // m/s*s
-double PI = 3.1415926;
-double iAngle = PI / 3.; // projectile inclination angle in radian
-double iSpeed = 25.0f;
+float PI = 3.1415926;
+float iAngle = PI / 3.; // projectile inclination angle in radian
+float iSpeed = 25.0f;
 int circleSections = 16;
-const double angleInc = PI / 180.;
+const float angleInc = PI / 180.f;
 
 float eyeX = 25.0f, eyeY = 10.0f, eyeZ = 100.0f;
 
 int winWidth = 800;
 int winHeight = 600;
-const double ratio = (float)winHeight / (float)winWidth;
-const double WorldWidth = 120.0; // meter wide
-const double WorldDepth = WorldWidth * ratio; // 
-const double WorldHeight = 100.;
+const float ratio = (float)winHeight / (float)winWidth;
+const float WorldWidth = 120.0; // meter wide
+const float WorldDepth = WorldWidth * ratio; // 
+const float WorldHeight = 100.;
 int width = 0, height = 0;
 
-// draw the slop /////////
-double radius = 1.;
-
-static double clocktime = 0.f;
+static float clocktime = 0.f;
 int framerate = 30;
 
 bool checkWindowResize();
-
+typedef Vector3d<float> MathVec;
+struct TracePoint
+{
+	MathVec position;
+	MathVec color;
+};
 struct Object3D
 {
-	Vector3d<double> pos, vel, acc;
+	MathVec pos, vel, acc;
 	int red, green, blue;
-	
-	double mass;
-	void set(double x, double y, double z, double m, Vector3d<double> v, int r, int g, int b)
+
+	float mass;
+	static const size_t maxPointCount = 400;
+	TracePoint posTrace[maxPointCount];
+	int traceCount;
+	TracePoint &SetNextTracePoint()
+	{
+		posTrace[traceCount].position = pos;
+		posTrace[traceCount].color = MathVec(255, 255, 255) - vel;
+		return posTrace[traceCount++];
+	}
+	Object3D() { traceCount = 0; }
+	void set(float x, float y, float z, float m, MathVec v, int r, int g, int b)
 	{
 		pos.x = x;
 		pos.y = y;
@@ -78,18 +87,33 @@ struct Object3D
 		green = r;
 		red = g;
 		blue = b;
+		traceCount = 0;
 	}
 
-	////////////////////////////////////////////////////////////////
-	void DrawAxis(float extend, bool running=false)
+	void DrawTrace()
 	{
 		glPushMatrix();
-		glLineWidth(3.f);
+		glLineWidth(2.f);
+		glBegin(GL_POINTS);
+		for (int i = 0; i < traceCount; i++)
+		{
+			auto &tp = posTrace[i];
+			glColor3i(tp.color.x, tp.color.y, tp.color.z);
+			glVertex3fv((float*)&tp.position);
+		}
+		glEnd();
+		glPopMatrix();
+	}
+	////////////////////////////////////////////////////////////////
+	void DrawAxis(float extend, bool running = false)
+	{
+		glPushMatrix();
+		glPointSize(3.f);
 		glMatrixMode(GL_MODELVIEW);
 		Matrix4 mModel, mView, mModelView;
 		static float angle = 0.f;
 		if (running)
-			angle += 0.3;
+			angle += 0.3f;
 		else
 			angle = 0.f;
 		// set rotation matrix for the frame to be rotaton around z axis by angle degrees
@@ -99,15 +123,15 @@ struct Object3D
 		mView.setColumn(0, T);
 		mView.setColumn(1, N);
 		mView.setColumn(2, B);
-		
+
 		// set translation to move the frame to where the object is.
 		mModel.translate(pos.x, pos.y, pos.z);
 		mModelView = mModel * mView;
 		// set the final matrix to be used as modelview matrix for opengl pipeline.
 		glMultMatrixf(mModelView.get());
 		glDisable(GL_LIGHTING);
-		glBegin(GL_LINES); 
-		glColor3f(1.f, 0.f, 0.f); 
+		glBegin(GL_LINES);
+		glColor3f(1.f, 0.f, 0.f);
 		glVertex3d(0.f, 0.f, 0.f);    // x axis
 		glVertex3d(extend, 0.f, 0.f);
 		glColor3f(0.f, 1.f, 0.f);
@@ -137,7 +161,7 @@ GLfloat *make_texture(int maxs, int maxt)
 	texture = (GLfloat *)malloc(maxs * maxt * sizeof(GLfloat));
 	for (t = 0; t < maxt; t++) {
 		for (s = 0; s < maxs; s++) {
-			texture[s + maxs * t] = 1.f-(((s >> 4) & 0x1) ^ ((t >> 4) & 0x1))*0.3f;
+			texture[s + maxs * t] = 1.f - (((s >> 4) & 0x1) ^ ((t >> 4) & 0x1))*0.3f;
 		}
 	}
 	return texture;
@@ -150,11 +174,11 @@ void initPhysics(double rad, double speed, double angle)
 	clocktime = 0.f;
 	double vx = speed * cos(angle);
 	double vy = speed * sin(angle);
-	double vz =0.f;
+	double vz = 0.f;
 	double initX = rad;
 	double inity = rad;
-	double initz = WorldDepth/4.f;
-	simBall.set(initX, inity, initz, 2, Vector3d<double>(vx, vy, vz), 128, 128, 0);
+	double initz = WorldDepth / 4.f;
+	simBall.set(initX, inity, initz, 2, MathVec(vx, vy, vz), 128, 128, 0);
 }
 
 int PollKeys()
@@ -170,29 +194,29 @@ int PollKeys()
 		keyRead = eStop;
 		break;
 	case FSKEY_UP:
-		friction += 0.1;
-		eyeY += 0.6;
+		//friction += 0.1;
+		eyeY += 0.6f;
 		break;
 	case FSKEY_DOWN:
-		friction = max(1., friction - 0.1);
-		eyeY -= 0.6;
+		//	friction = max(1., friction - 0.1);
+		eyeY -= 0.6f;
 		break;
 	case FSKEY_LEFT:
 		iAngle = max(0., iAngle - angleInc);
-		eyeX -= 0.6;
+		eyeX -= 0.6f;
 		break;
 	case FSKEY_RIGHT:
-		iAngle = min(90.0, iAngle + angleInc);
-		eyeX += 0.6;
+		iAngle = min(90.0f, iAngle + angleInc);
+		eyeX += 0.6f;
 		break;
 	case FSKEY_PAGEDOWN:
-		iSpeed = max(iSpeed - 5.0, 0.0);
+		iSpeed = max(iSpeed - 5.0f, 0.0f);
 		break;
 	case FSKEY_PAGEUP:
-		iSpeed = min(iSpeed + 5.0, 100.);
+		iSpeed = min(iSpeed + 5.0f, 100.f);
 		break;
 	case FSKEY_I:
-		eyeY = min(eyeY + 1.0, 100.0);
+		eyeY = min(eyeY + 1.0f, 100.0f);
 		break;
 	case FSKEY_K:
 		eyeY = max(eyeY - 1.0, 1.);
@@ -212,9 +236,8 @@ int Menu(void)
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(-0.5, (GLdouble)width - 0.5, (GLdouble)height - 0.5, -0.5, -1, 1);
-
 	glClearColor(0.0, 0.0, 0.0, 0.0);
-	
+
 	while (key != eStart && key != eStop)
 	{
 		key = PollKeys();
@@ -226,13 +249,13 @@ int Menu(void)
 		// printing UI message info
 		glColor3f(1., 1., 1.);
 		char msg[128];
-		sprintf_s(msg, "Friction is %f. Use Up/Down keys to change it by 1/10!\n", friction);
-		glRasterPos2i(32, 32);
-		glCallLists(strlen(msg), GL_UNSIGNED_BYTE, msg);
+		//sprintf_s(msg, "Friction is %f. Use Up/Down keys to change it by 1/10!\n", friction);
+		//glRasterPos2i(32, 32);
+		//glCallLists(strlen(msg), GL_UNSIGNED_BYTE, msg);
 
 		sprintf_s(msg, "Slope Angle is %f degrees. Use Left/Right keys to change it!\n", iAngle*180. / PI);
-		glRasterPos2i(32,64);
-		glCallLists(strlen(msg),GL_UNSIGNED_BYTE, msg);
+		glRasterPos2i(32, 64);
+		glCallLists(strlen(msg), GL_UNSIGNED_BYTE, msg);
 
 		sprintf_s(msg, "Projectile speed is %f m/s. Use PageUp/PageDown keys to change it!\n", iSpeed);
 		glRasterPos2i(32, 96);
@@ -243,17 +266,17 @@ int Menu(void)
 		glCallLists(strlen(msg), GL_UNSIGNED_BYTE, msg);
 
 		const char *msg1 = "S.....Start Game";
-		const char *msg2="ESC...Exit";
+		const char *msg2 = "ESC...Exit";
 		glRasterPos2i(32, 160);
-		glCallLists(strlen(msg1),GL_UNSIGNED_BYTE, msg1);
-		glRasterPos2i(32,192);
-		glCallLists(strlen(msg2),GL_UNSIGNED_BYTE, msg2);
+		glCallLists(strlen(msg1), GL_UNSIGNED_BYTE, msg1);
+		glRasterPos2i(32, 192);
+		glCallLists(strlen(msg2), GL_UNSIGNED_BYTE, msg2);
 
 		FsSwapBuffers();
 		FsSleep(10);
 	}
 
-	initPhysics(radius, iSpeed, iAngle);
+	initPhysics(1.f, iSpeed, iAngle);
 	return key;
 }
 
@@ -273,7 +296,7 @@ void renderScene(bool reset)
 
 	// set the camera
 	gluLookAt(eyeX, eyeY, eyeZ, 20., 0., 0., 0.0f, 1.0f, 0.0f);
-	
+
 	//////////////////// draw the ground ///////////////
 	glEnable(GL_TEXTURE_2D);
 	glBegin(GL_QUADS);
@@ -290,7 +313,9 @@ void renderScene(bool reset)
 	glEnd();
 	glDisable(GL_TEXTURE_2D);
 
+	// draw the frame and its trace:
 	simBall.DrawAxis(2.f, reset);
+	simBall.DrawTrace();
 
 	// draw walls:
 	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, wall_mat);
@@ -304,10 +329,10 @@ void renderScene(bool reset)
 
 	/* ceiling */
 	glNormal3f(0.f, -1.f, 0.f);
-	glVertex3f(0.f, 100.f, WorldHeight/2.);
+	glVertex3f(0.f, 100.f, WorldHeight / 2.);
 	glVertex3f(0.f, 100.f, 0.f);
 	glVertex3f(WorldWidth, 100.f, 0.f);
-	glVertex3f(WorldWidth, 100.f, WorldHeight/2.);
+	glVertex3f(WorldWidth, 100.f, WorldHeight / 2.);
 
 	/* back wall */
 	glNormal3f(0.f, 0.f, 1.f);
@@ -315,11 +340,13 @@ void renderScene(bool reset)
 	glVertex3f(WorldWidth, 0.f, 0.f);
 	glVertex3f(WorldWidth, 100.f, 0.f);
 	glVertex3f(0.f, 100.f, 0.f);
-	
+
 	glEnd();
 
 	FsSwapBuffers();
 }
+
+const float gravity = 9.81f;
 /////////////////////////////////////////////////////////////////////
 void updatePhysics(Object3D &ball, double timeInc)
 {
@@ -331,12 +358,16 @@ void updatePhysics(Object3D &ball, double timeInc)
 	ball.pos.y += ball.vel.y * timeInc; // y position update
 	ball.pos.z += ball.vel.z * timeInc; // y position update
 
-	ball.vel.y -= gravity * timeInc; // y speed update
+	ball.vel.y -= gravity * timeInc;    // y speed update
+
+	//update trace
+	TracePoint &tp = ball.SetNextTracePoint();
+	std::cout << "Pos(" << ball.traceCount << ":" << ball.pos.x << "," << ball.pos.y << "," << ball.pos.z << "==" << tp.position.x << "," << tp.position.y << "," << tp.position.z << std::endl;
 }
 
 void resetPhysics()
 {
-	initPhysics(radius, iSpeed, iAngle);
+	initPhysics(1.f, iSpeed, iAngle);
 }
 
 bool checkWindowResize()
@@ -352,7 +383,6 @@ bool checkWindowResize()
 	return false;
 }
 
-
 ///////////////////////////////////////////////////////////////////
 int Game(void)
 {
@@ -363,7 +393,7 @@ int Game(void)
 	glEnable(GL_LIGHT0);
 
 	/* place light 0 in the right place */
-	GLfloat lightpos[] = { WorldWidth/2.f, 50.f, WorldHeight/2.f, 1.f };
+	GLfloat lightpos[] = { WorldWidth / 2.f, 50.f, WorldHeight / 2.f, 1.f };
 	glLightfv(GL_LIGHT0, GL_POSITION, lightpos);
 
 	/* remove back faces to speed things up */
@@ -378,10 +408,10 @@ int Game(void)
 	glTexImage2D(GL_TEXTURE_2D, 0, 1, TEXDIM, TEXDIM, 0, GL_RED, GL_FLOAT, tex);
 	free(tex);
 
-//	int lb, mb, rb, mx, my;
+	//	int lb, mb, rb, mx, my;
 	DWORD passedTime = 0;
 	FsPassedTime(true);
-	
+
 	//////////// initial setting up the scene ////////////////////////////////////////
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -389,29 +419,28 @@ int Game(void)
 	checkWindowResize();
 
 	// set the camera
-	float ratio = width / height;
+	float ratio = (float)width / (float)height;
 	gluPerspective(45.f, ratio, 0.1f, 150.f);
-	
 	int key = eIdle;
 
 	glMatrixMode(GL_MODELVIEW);
 	bool resetFlag = false;
-	while(1)
+	while (1)
 	{
 		if (checkWindowResize())
 		{
-			float ratio = width / height;
+			ratio = (float)width / (float)height;
 			gluPerspective(50.f, ratio, 0.1f, 100.f);
 
 		}
 		//FsGetMouseState(lb,mb,rb,mx,my);
-		key = PollKeys(); 
-		if(key == eStop)
+		key = PollKeys();
+		if (key == eStop)
 			break;
 		if (key == eStart)
 			resetFlag = false;
 
-		timeInc = (double)(passedTime) * 0.001;
+		timeInc = (double)(passedTime)* 0.001;
 		clocktime += timeInc;
 		/////////// update physics /////////////////
 		if (simBall.pos.y < -0.01)
@@ -419,7 +448,7 @@ int Game(void)
 			resetPhysics();
 			resetFlag = true;
 		}
-		
+
 		if (!resetFlag)
 			updatePhysics(simBall, timeInc);
 		/////////////////////////////////////////
@@ -427,16 +456,16 @@ int Game(void)
 
 		////// update time lapse /////////////////
 		passedTime = FsPassedTime(); // Making it up to 50fps
-		int timediff = timeSpan-passedTime;
-	//	printf("\ntimeInc=%f, passedTime=%d, timediff=%d", timeInc, passedTime, timediff);
-		while(timediff >= timeSpan/3)
+		int timediff = timeSpan - passedTime;
+		//	printf("\ntimeInc=%f, passedTime=%d, timediff=%d", timeInc, passedTime, timediff);
+		while (timediff >= timeSpan / 3)
 		{
 			FsSleep(5);
-			passedTime=FsPassedTime(); // Making it up to 50fps
-			timediff = timeSpan-passedTime;
-	//		printf("--passedTime=%d, timediff=%d", passedTime, timediff);
+			passedTime = FsPassedTime(); // Making it up to 50fps
+			timediff = timeSpan - passedTime;
+			//		printf("--passedTime=%d, timediff=%d", passedTime, timediff);
 		}
-		passedTime=FsPassedTime(true); // Making it up to 50fps
+		passedTime = FsPassedTime(true); // Making it up to 50fps
 	}
 	return key;
 }
@@ -454,11 +483,10 @@ int main(void)
 	glDisable(GL_DEPTH_TEST);
 	glDisable(GL_LIGHTING);
 
-	menu=Menu();
+	menu = Menu();
 	if (Menu() != eStop)
 		Game();
-	
+
 	return 0;
 }
-
 
--- src/vector3d.h
+++ src/vector3d.h
@@ -9,7 +9,8 @@
 y value. Certain operators are overloaded to make it easier
 for vector math to be performed.*/
 template<typename T>
-class Vector3d {
+class Vector3d
+{
 public:
 	/*The x and y values are public to give easier access for
 	outside funtions. Accessors and mutators are not really
@@ -25,9 +26,13 @@ public:
 	/*The following operators simply return Vector3ds that
 	have operations performed on the relative (x, y) values*/
 	Vector3d& operator+=(const Vector3d& v) { x += v.x; y += v.y; z += v.z; return *this; }
+	Vector3d& operator+=(const T a) { x += a; y += a; z += a; return *this; }
 	Vector3d& operator-=(const Vector3d& v) { x -= v.x; y -= v.y; z -= v.z; return *this; }
+	Vector3d& operator-=(const T a) { x -= a; y -= a; z -= a; return *this; }
 	Vector3d& operator*=(const Vector3d& v) { x *= v.x; y *= v.y; z *= v.z; return *this; }
+	Vector3d& operator*=(const T a) { x *= a; y *= a; z *= a; return *this; }
 	Vector3d& operator/=(const Vector3d& v) { x /= v.x; y /= v.y; z /= v.z; return *this; }
+	Vector3d& operator/=(const T a) { x /= a; y /= a; z /= a; return *this; }
 
 	//Check if the Vectors have the same values (uses pairwise comparison of 
 	// 'std::tuple' on the x, y values of L and R.)
@@ -47,56 +52,60 @@ public:
 	Vector3d operator-() const { return Vector3d(-x, -y, -z); }
 
 	//Apply scalar operations.
-	Vector3d& operator*(T s) { x *= s; y *= s; z *= s;  return *this; }
-	Vector3d& operator/(T s) { x /= s; y /= s; z /= s;  return *this; }
+	Vector3d operator*(T s) { return Vector3d<T>(*this) *= s; }
+	Vector3d operator/(T s) { return Vector3d<T>(*this) /= s; }
 
 	//Returns the length of the vector from the origin.
 	double Length() const { return sqrt(x*x + y*y, z*z); }
-	double LengthSq()const { return x*x + y*y +z*z; }
+	double LengthSq()const { return x*x + y*y + z*z; }
 
 };
 
-template<class T> Vector3d<T> operator*(const T& s, const Vector3d<T>& v) { return Vector3d<T>(v) *= s; }
-template<class T> Vector3d<T> operator*(const Vector3d<T>& v, const T& s) { return Vector3d<T>(v) *= s; }
+template<class T> Vector3d<T> operator*(const T s, const Vector3d<T>& v) { return Vector3d<T>(v) *= s; }
+template<class T> Vector3d<T> operator*(const Vector3d<T>& v, const T s) { return Vector3d<T>(v) *= s; }
 
-template<class T> Vector3d<T>  operator-(const Vector3d<T>& v1, const Vector3d<T>& v2) { return Vector3d<T>(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z); }
-template<class T> Vector3d<T>  operator+(const Vector3d<T>& v1, const Vector3d<T>& v2) { return Vector3d<T>(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z); }
+template<class T> Vector3d<T>  operator-(const Vector3d<T>& v1, const Vector3d<T>& v2) { return Vector3d<T>(v1) -= v2; }
+template<class T> Vector3d<T>  operator+(const Vector3d<T>& v1, const Vector3d<T>& v2) { return Vector3d<T>(v1) += v2; }
+
+template<class T> Vector3d<T>  operator/(const Vector3d<T>& v1, const T a) { return Vector3d<T>(v1) /= a; }
 
 //Product functions
 template<class T> T DotProduct(const Vector3d<T>& a, const Vector3d<T>& b) { return  ((a.x * b.x) + (a.y * b.y) + (a.z * b.z)); }
-//template<class T>  Vector3d<T>  CrossProduct(const Vector3d<T>& a, const Vector3d<T>& b) { return ((a.x * b.y) - (a.y * b.x)); }
+template<class T>  Vector3d<T>  CrossProduct(const Vector3d<T>& a, const Vector3d<T>& b) {
+	return Vector3d<T>((a.y*b.z - a.z - b.y), (a.z*b.x - a.x*b.z), (a.x*b.y - a.y*b.x));
+}
 
 //Return the unit vector of the input
-template<class T> Vector3d<T> Normal(const Vector3d<T>& a) { double mag = a.Length(); return v<T>(a.x / mag, a.y / mag, a.z/mag); }
+template<class T> Vector3d<T> Normal(const Vector3d<T>& a) { double mag = a.Length(); return v<T>(a.x / mag, a.y / mag, a.z / mag); }
 
 //Return a vector perpendicular to the left.
 //template<class T> Vector3d<T> Perpendicular(const Vector3d<T>& a) { return Vector3d<T>(a.y, -a.x); }
 //Return true if two line segments intersect.
 /*template<class T> bool Intersect(const Vector3d<T>&aa, const Vector3d<T>& ab, const Vector3d<T>& ba, const Vector3d<T>& bb)
 {
-	Vector3d<T> p = aa;
-	Vector3d<T> r = ab - aa;
-	Vector3d<T> q = ba;
-	Vector3d<T> s = bb - ba;
+Vector3d<T> p = aa;
+Vector3d<T> r = ab - aa;
+Vector3d<T> q = ba;
+Vector3d<T> s = bb - ba;
 
-	double t = CrossProduct((q - p), s) / CrossProduct(r, s);
-	double u = CrossProduct((q - p), r) / CrossProduct(r, s);
+double t = CrossProduct((q - p), s) / CrossProduct(r, s);
+double u = CrossProduct((q - p), r) / CrossProduct(r, s);
 
-	return (0.0 <= t && t <= 1.0) &&
-		(0.0 <= u && u <= 1.0);
+return (0.0 <= t && t <= 1.0) &&
+(0.0 <= u && u <= 1.0);
 }
 */
 //Return the point where two lines intersect.
 /*template<class T> Vector3d<T> GetIntersect(const Vector3d<T>&aa, const Vector3d<T>& ab, const Vector3d<T>& ba, const Vector3d<T>& bb)
 {
-	double pX = (aa.x*ab.y - aa.y*ab.x)*(ba.x - bb.x) -
-		(ba.x*bb.y - ba.y*bb.x)*(aa.x - ab.x);
-	double pY = (aa.x*ab.y - aa.y*ab.x)*(ba.y - bb.y) -
-		(ba.x*bb.y - ba.y*bb.x)*(aa.y - ab.y);
-	double denominator = (aa.x - ab.x)*(ba.y - bb.y) -
-		(aa.y - ab.y)*(ba.x - bb.x);
+double pX = (aa.x*ab.y - aa.y*ab.x)*(ba.x - bb.x) -
+(ba.x*bb.y - ba.y*bb.x)*(aa.x - ab.x);
+double pY = (aa.x*ab.y - aa.y*ab.x)*(ba.y - bb.y) -
+(ba.x*bb.y - ba.y*bb.x)*(aa.y - ab.y);
+double denominator = (aa.x - ab.x)*(ba.y - bb.y) -
+(aa.y - ab.y)*(ba.x - bb.x);
 
-	return Vector3d(pX / denominator, pY / denominator);
+return Vector3d(pX / denominator, pY / denominator);
 }
 */
 #endif
